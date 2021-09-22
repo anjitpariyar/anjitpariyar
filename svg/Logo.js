8f8ed0cfaf5303a5936a4320a7716963fd416b88
anjitpariyar <anjitpariyar@gmail.com>
2021-09-22 22:22:56 +0545

    footor logo changed

--- svg/Logo.js
+++ svg/Logo.js
@@ -2,56 +2,26 @@ const Logo = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="789.872"
-      height="793.121"
-      viewBox="0 0 789.872 793.121"
+      width="708.306"
+      height="708.306"
+      viewBox="0 0 708.306 708.306"
     >
-      <g id="Group_44" dataname="Group 44" transform="translate(-1397 -1052)">
-        <g id="document" transform="translate(1751.153 1406.153)">
-          <path
-            id="Path_34"
-            dataname="Path 34"
-            d="M176.087,178.066c1.2-1.249,2.018-1.977,1.979-1.979L354.153,0,0-354.153-354.153,0,0,354.153Z"
-          />
-          <path
-            id="Path_35"
-            dataname="Path 35"
-            d="M282.98,354.153a71.173,71.173,0,1,0-71.172-71.173A71.142,71.142,0,0,0,282.98,354.153Z"
-            transform="translate(-282.98 -435.98)"
-            fill="#fff"
-          />
-        </g>
-        <g
-          id="Group_43"
-          dataname="Group 43"
-          transform="translate(1807.236 1663.94) rotate(-42)"
-        >
-          <g id="Group_40" dataname="Group 40" transform="translate(75.726 93)">
-            <g id="Group_39" dataname="Group 39">
-              <path
-                id="Path_37"
-                dataname="Path 37"
-                d="M152.831,0c-42.516,0-77.1,35.132-77.1,78.313v62.363c0,43.181,34.588,78.311,77.1,78.311s77.1-35.023,77.1-78.071v-62.6C229.936,35.132,195.345,0,152.831,0Zm0,14.5Z"
-                transform="translate(-75.726)"
-              />
-            </g>
-          </g>
-          <g
-            id="Group_42"
-            dataname="Group 42"
-            transform="translate(145.579 113.166)"
-          >
-            <g id="Group_41" dataname="Group 41">
-              <path
-                id="Path_38"
-                dataname="Path 38"
-                d="M246.3,140.15a7.251,7.251,0,0,0-7.251,7.251v25.379a7.251,7.251,0,0,0,14.5,0V147.4A7.251,7.251,0,0,0,246.3,140.15Z"
-                transform="translate(-239.046 -140.15)"
-                fill="#fff"
-              />
-            </g>
-          </g>
-        </g>
+      <g id="Group_44" data-name="Group 44" transform="translate(-1397 -1052)">
+        <path
+          id="Intersection_1"
+          data-name="Intersection 1"
+          d="M-3759,1180.154-3404.847,826l354.153,354.153s-71.394,70.745-131.1,131.171c-64.03-64.78-139.8-13.532-139.844,47.255-.017,21.467,9.411,44.122,32.131,63.278-52.742,51.826-115.344,112.449-115.344,112.449Z"
+          transform="translate(5156 226)"
+        />
+        <path
+          id="Path_35"
+          data-name="Path 35"
+          d="M282.98,354.153a71.173,71.173,0,1,0-71.172-71.173A71.142,71.142,0,0,0,282.98,354.153Z"
+          transform="translate(1468.173 970.173)"
+          fill="#fff"
+          stroke="#000"
+          strokeWidth="2"
+        />
       </g>
     </svg>
   );
