20da74b1d565a62aba514eb3c629e5d7d10aca53
anjitpariyar <anjitpariyar@gmail.com>
2021-10-02 23:52:44 +0545

    goole anylatics and hotjar

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Logo from "../svg/Logo";
 import Hamburger from "../svg/Hamburger";
 import Navigation from "./Navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Head from "next/head";
 
@@ -14,6 +14,27 @@ const Header = () => {
     e.preventDefault();
     setShow(state);
   };
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-7KQ8BZH8SV");
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 1530972, hjsv: 6 };
+      a = o.getElementsByTagName("head")[0];
+      r = o.createElement("script");
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
+  });
   return (
     <>
       <Head>
@@ -121,6 +142,10 @@ Creating a Web."
           property="og:url"
           content="https://anjitpariyar.github.io/"
         ></meta>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7KQ8BZH8SV"
+        ></script>
       </Head>
       <header className={styles.header}>
         <div className={styles.container}>
