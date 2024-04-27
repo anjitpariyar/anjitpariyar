a56315e235563e3c7ede9204c5e6b6ac9f5857d0
anjit pariyar <anjitpariyar@gmail.com>
2024-04-27 11:10:10 +0545

    update: site map

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,19 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://bhoomi.com.np",
+  siteUrl: process.env.SITE_URL || "https://www.anjitpariyar.com.np",
   generateRobotsTxt: true, // (optional)
+  exclude: ["/404"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/404"],
+      },
+      { userAgent: "*", allow: "/" },
+    ],
+    additionalSitemaps: [
+      `${process.env.NEXT_PUBLIC_SITE_URL}sitemap.xml`,
+      `${process.env.NEXT_PUBLIC_SITE_URL}server-sitemap.xml`,
+    ],
+  },
   // ...other options
 };
